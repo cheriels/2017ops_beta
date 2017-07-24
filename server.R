c8f3980ce274ad07ab24f87b3b5a9d82b3786245
Zachary Smith <zsmith@ICPRB.INT>
2017-07-24 12:22:48 -0400

    Zach's updates...

---
+++ server.R
@@ -0,0 +1,80 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+#library(data.table)
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  output$plot <- renderPlot({
+    #--------------------------------------------------------------------------
+    todays.date <- as.Date(input$today.override)
+    start.date <- as.Date(input$date.range1[1])
+    end.date <- as.Date(input$date.range1[2])
+    #--------------------------------------------------------------------------
+    # Add recessions and lags - want to do this using function in future!
+    upstr.df <- flows.df %>% 
+      select(date, lfalls, por, monocacy) %>% 
+      lag_k(por, todays.date, start.date, lag.days = 1)
+    #str(upstr)
+    #--------------------------------------------------------------------------
+    # recess and lag Monocacy flows
+    upstr.df <- upstr.df %>% 
+      lag_k(monocacy, todays.date, start.date, lag.days = 1) %>% 
+      # Predict Little Falls from POR and Monocacy
+      mutate(lfalls_from_upstr = por_recess_lag + monocacy_recess_lag) %>% 
+      select(date, lfalls, por, lfalls_from_upstr) %>% 
+      filter(date > start.date & date < end.date) %>% 
+      tidyr::gather(gage, flow, lfalls:lfalls_from_upstr) %>% 
+      na.omit()
+      
+    #--------------------------------------------------------------------------
+    # plot flows
+    final.plot <- ggplot(upstr.df, aes(x = date, y = flow,
+                                       color = gage, size = gage,
+                                       linetype = gage)) + 
+      
+      geom_line() +
+      # Has to be in alphabetical order
+      scale_size_manual(name = "Station",
+                        breaks = c("lfalls", "lfalls_from_upstr", "por"), 
+                        values = c(2, 1, 1)) + 
+      # Has to be in alphabetical order
+      scale_linetype_manual(name = "Station",
+                            breaks = c("lfalls", "lfalls_from_upstr", "por"),
+                            values = c("solid", "dashed", "solid")) +
+      # Has to be in alphabetical order
+      scale_colour_manual(name = "Station",
+                          breaks = c("lfalls", "lfalls_from_upstr", "por"),
+                          values = c("#0072B2", "#56B4E9", "#E69F00")) +
+      
+      theme_minimal() +
+      xlab("Date") +
+      ylab("Flow (CFS)") +
+      geom_hline(yintercept = 2000, color = "red",
+                 linetype = "longdash", size = 0.7)
+    
+    final.plot
+    #--------------------------------------------------------------------------
+    
+    # plot flows
+    #    flows <- subset(flows, date > as.Date(input$daterange1[1]) & date < as.Date(input$daterange1[2]))
+    #    ggplot(flows, aes(x=date)) + geom_line(aes(y=lfalls, color="Little Falls"), size=2) +
+    #          geom_line(aes(y=por, color="Point of Rocks"), size=1) +
+    #          geom_line(aes(y=monocacy, color="Monocacy"), size=1) +
+    #          scale_colour_manual(name="Station", breaks = c("Little Falls","Point of Rocks", "Monocacy"), values=c("skyblue1","green","blue"))
+    
+    
+    
+  }) # End output$plot
+  
+})
---
+++ ui.R
@@ -0,0 +1,48 @@
+#
+# This is the user-interface definition of a first try Shiny web application to
+#    replace CO-OP's Excel tools, DroughtOps_Daily.xlsx and DroughtOps_Hourly.xlsx.
+#
+# To get this to run - from Console - type
+#   > setwd("c:/workspace/2017ops_beta/scripts")
+#   > library(shiny)
+#
+# Then you can run the shiny app by typing
+#   > runApp()
+#
+# To close the app, hit the red button on the upper right of Console.
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  # The first tab, Situational Awareness. is meant to give ops staff a first look at where we are today
+  # and what needs to be done.
+  # Right now I'm working on a graph of Little Falls flow, predicted Little Falls flow, and Point of Rocks flow.
+  # I want to add to the graph the Point of Rocks trigger for daily monitoring,
+  # and the Little Falls trigger for enhanced drought operations.
+  #----------------------------------------------------------------------------
+  fluidRow(
+    # Title of first tab
+    column(width = 6,
+           h4("Situational Awareness"))
+  ), # End fluidRow
+  #----------------------------------------------------------------------------
+  fluidRow(
+    # Sidebar with a slider input for number of bins 
+    column(width = 6,
+           dateInput("today.override","Optional override of today's date:"),
+           dateRangeInput("date.range1",
+                          "Date range for graphs:", 
+                          start = Sys.Date() - 30,
+                          end = Sys.Date() + 15)),
+    column(width = 6, tags$b(paste("Today's date is ", Sys.Date())))
+  ), # End fluidRow
+  #----------------------------------------------------------------------------
+  fluidRow(
+    # Show a plot of the generated distribution
+    textOutput("This is a test"),
+    plotOutput("plot")
+  ) # End fluidRow
+) # End fluidPage
+) # End shinyUI
+
