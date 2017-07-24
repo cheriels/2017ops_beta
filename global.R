1c4ee932015a4b4bf261910c5748579e06eaf812
Zachary Smith <zsmith@ICPRB.INT>
2017-07-24 13:21:13 -0400

    Moved the lag-k function to the constant_lagk.R script because that function is only associated with constant lag-K calculations.

--- global.R
+++ global.R
@@ -11,27 +11,4 @@ flows.df <- read.csv("data/flow_daily_cfs.csv", stringsAsFactors = FALSE) %>%
 #------------------------------------------------------------------------------
 
 
-lag_k <- function(flow.df, gage, todays.date, start.date, lag.days = 1) {
-  gage <- rlang::enquo(gage)
-  gage.string <- rlang::quo_name(gage)
-  if (gage.string == "por") gage.k <- 0.042
-  if (gage.string == "monocacy") gage.k <- 0.050
-  #----------------------------------------------------------------------------
-  final.df <- flow.df %>% 
-    # recess and lag POR flows
-    mutate(flow1 = lag(rlang::UQ(gage), n = 1),
-           flow2 = lag(rlang::UQ(gage), n = 2),
-           recess_init = pmin(rlang::UQ(gage), flow1, flow2), # pmin gives minimums of each row
-           recess_time = ifelse(date - todays.date < 0, 0, date - todays.date),
-           #recess_time = case_when(
-           #  date - todays.date < 0 ~ 0,
-           #  TRUE ~ date - today.date
-           #),
-           recess = ifelse(date < todays.date, rlang::UQ(gage), 
-                               recess_init[todays.date - start.date + 1] * exp(-gage.k * recess_time)),
-           recess_lag = lag(recess, lag.days)) %>% 
-    select(-flow1, -flow2, - recess_init, -recess_time, - recess)
-  
-  names(final.df)[names(final.df) %in% "recess_lag"] <- paste(gage.string, "recess_lag", sep = "_")
-  return(final.df)
-}
+
