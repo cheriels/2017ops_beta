93f5421ae9dd501f03e5346ceddf6339bdca843c
zsmith27 <zsmith@icprb.org>
2017-07-25 13:42:57 -0400

    Import MARFC data.

--- global.R
+++ global.R
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------------------
-# read flows:
+# Import daily flow data.
 flows.df <- read.csv("data/flow_daily_cfs.csv", stringsAsFactors = FALSE) %>% 
   dplyr::select(1:6) %>% 
   dplyr::rename(lfalls = X1646500,
@@ -9,6 +9,12 @@ flows.df <- read.csv("data/flow_daily_cfs.csv", stringsAsFactors = FALSE) %>%
                 por = X1638500) %>% 
   dplyr::mutate(date = as.Date(date))
 #------------------------------------------------------------------------------
-
+# Import marfc data.
+marfc.df <- data.table::fread("data/marfc_brkm2.csv", data.table = FALSE) %>% 
+  dplyr::mutate(date_time = lubridate::mdy_hm(date_time),
+                stage = as.numeric(gsub("ft", "", stage)),
+                state_units = "ft",
+                flow = as.numeric(gsub("kcfs", "", flow)) * 1000,
+                flow_units = "cfs")
 
 
