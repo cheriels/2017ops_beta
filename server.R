fd9396a283d558776a26b2317eebb0b85780f91d
zsmith27 <zsmith@icprb.org>
2017-07-28 09:52:19 -0400

    Automatic session end appears to work when the UI is subdivided into sourced scripts. The previous eror messages no longer appear in the console.

--- server.R
+++ server.R
@@ -12,7 +12,7 @@ shinyServer(function(input, output, session) {
   # Automatically stop session once browser window is closed.
   # Appears to work but when editing the ui.R errors will beging to appear in
   # to console.
-  #session$onSessionEnded(stopApp)
+  session$onSessionEnded(stopApp)
   #----------------------------------------------------------------------------
   # TAB 1
   # Situational Awareness (sa)
