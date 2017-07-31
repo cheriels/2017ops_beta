61482e6bd9ce38bd6833fd6bd4506ff559296700
zsmith27 <zsmith@icprb.org>
2017-07-31 16:01:20 -0400

    A function to create a standardized time series. Ultimately allows the user to fill time gaps and subsequently use the zoo package to interpolate.

--- functions/date_standards_func.R
+++ functions/date_standards_func.R
@@ -7,3 +7,9 @@ date_standards <- function(name, session, start.date, end.date, min.range) {
   }
   updateDateRangeInput(session, name, start = start.date, end = end.date)
 }
+#------------------------------------------------------------------------------
+date_frame <- function(start.date, end.date, seq.by = "hour") {
+    
+  data.frame(date_time = seq.POSIXt(start.date, end.date, by = seq.by))
+}
+#------------------------------------------------------------------------------
