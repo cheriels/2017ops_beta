a801741f2c4b2cd0b719bd2b104e3f6a20284279
zsmith27 <zsmith@icprb.org>
2017-07-31 16:02:37 -0400

    Updated to handle tidy data format (long data).

--- functions/variable_lagk_func.R
+++ functions/variable_lagk_func.R
@@ -1,43 +1,36 @@
 
 variable_lagk <- function(long.df, gage.name, por.lag, klag.df) {
   #------------------------------------------------------------------------------
-  gage.name = rlang::enquo(gage.name)
-  #------------------------------------------------------------------------------
   klag.sub <- klag.df %>% 
     dplyr::filter(gage == por.lag) %>% 
     dplyr::select(flow, lag)
   #------------------------------------------------------------------------------
-  flow.df <- long.df %>% 
-    dplyr::mutate(range = as.character(cut(rlang::UQ(gage.name), 
-                                           breaks = klag.sub$flow)),
+  predicted.df <- long.df %>% 
+    dplyr::filter(gage == gage.name) %>% 
+    dplyr::mutate(range = as.character(cut(flow, breaks = klag.sub$flow)),
                   lower_flow = as.numeric(gsub("\\(|,.*", "", range)),
                   upper_flow = as.numeric(gsub(".*,|\\]", "", range))) %>% 
     dplyr::left_join(klag.sub, by = c("lower_flow" = "flow")) %>% 
     dplyr::left_join(klag.sub, by = c("upper_flow" = "flow")) %>% 
     dplyr::rename(lower_lag = lag.x,
                   upper_lag = lag.y) %>% 
-    dplyr::mutate(lag = lower_lag + ((upper_lag - lower_lag) * (rlang::UQ(gage.name) - lower_flow) / (upper_flow - lower_flow)),
-                  lag = date_time + lag * 3600)
+    dplyr::mutate(lag = lower_lag + ((upper_lag - lower_lag) * (flow - lower_flow) / (upper_flow - lower_flow)),
+                  lag = date_time + lag * 3600) %>% 
+    dplyr::select(lag, gage, flow) %>% 
+    dplyr::rename(date_time = lag) %>% 
+    dplyr::mutate(gage = "predicted") %>% 
+    dplyr::mutate(date_time = as.POSIXct(round(date_time, units = "hours"))) %>% 
+    dplyr::group_by(date_time, gage) %>% 
+    dplyr::summarise(flow = mean(flow))
   #------------------------------------------------------------------------------
-  lag.df <- dplyr::select(flow.df, rlang::UQ(gage.name), lag) %>% 
-    dplyr::filter(!is.na(lag)) %>% 
-    dplyr::mutate(lag = as.POSIXct(round(lag, units = "hours"))) %>% 
-    dplyr::rename(date_time = lag,
-                  predicted = rlang::UQ(gage.name)) %>% 
-    dplyr::group_by(date_time) %>% 
-    dplyr::summarise(predicted = mean(predicted))
-  #------------------------------------------------------------------------------
-  final.df <- flow.df %>% 
-    dplyr::select(-lag) %>% 
-    dplyr::full_join(lag.df, by = "date_time") %>% 
-    dplyr::mutate(predicted = c(rep(NA, which.min(is.na(predicted)) - 1),
-                                zoo::na.approx(predicted))) %>% #,
-#                  residual = rlang::UQ(gage.name) - predicted,
-#                  rmse = sqrt(mean(residual ^ 2, na.rm = TRUE))) %>% 
-    dplyr::select(-range, -lower_flow, - upper_flow, - lower_lag, -upper_lag)
+  start.date <- min(predicted.df$date_time)
+  end.date <- max(predicted.df$date_time)
+  date.frame <- date_frame(start.date, end.date, "hour")
+  final.df <- left_join(date.frame, predicted.df, by = "date_time") %>% 
+    mutate(flow = c(rep(NA, which.min(is.na(flow)) - 1),
+                         zoo::na.approx(flow)),
+           gage = "predicted") %>% 
+    dplyr::filter(!is.na(flow))
   #------------------------------------------------------------------------------
   return(final.df)
 }
-
-
-
