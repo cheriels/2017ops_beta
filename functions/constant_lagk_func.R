e57a400c25f31c4f3299fa45a29a4003a58e6a7d
Zachary Smith <zsmith@ICPRB.INT>
2017-09-12 12:45:53 -0400

    The constant_lagk function was updated to handle data in a long data format.

--- functions/constant_lagk_func.R
+++ functions/constant_lagk_func.R
@@ -5,19 +5,28 @@ constant_lagk <- function(flow.df, gage, todays.date, lag.days = 1) {
   gage <- rlang::enquo(gage)
   gage.string <- rlang::quo_name(gage)
   if (gage.string == "por") gage.k <- 0.042
-  if (gage.string == "monocacy") gage.k <- 0.050
+  if (gage.string == "mon_jug") gage.k <- 0.050
   #----------------------------------------------------------------------------
-  final.df <- flow.df %>% 
+  lag.df <- flow.df %>% 
+    dplyr::select(date_time, site, flow) %>% 
+    dplyr::filter(site %in% gage.string) %>% 
     # recess and lag POR flows
-    mutate(flow1 = lag(rlang::UQ(gage), n = 1),
-           flow2 = lag(rlang::UQ(gage), n = 2),
-           recess_init = pmin(rlang::UQ(gage), flow1, flow2), # pmin gives minimums of each row
+    mutate(flow1 = lag(flow, n = 1),
+           flow2 = lag(flow, n = 2),
+           recess_init = pmin(flow, flow1, flow2), # pmin gives minimums of each row
            recess_time = ifelse(date_time - todays.date < 0, 0, date_time - todays.date),
-           recess = ifelse(date_time < todays.date, rlang::UQ(gage), 
+           recess = ifelse(date_time < todays.date, flow, 
                            recess_init[date_time == todays.date] * exp(-gage.k * recess_time)),
            recess_lag = lag(recess, lag.days)) %>% 
-    select(-flow1, -flow2, - recess_init, -recess_time, - recess)
+    select(-flow1, -flow2, - recess_init, -recess_time, - recess, - site) %>% 
+    tidyr::gather(site, flow, flow:recess_lag) %>% 
+    dplyr::mutate(
+      site = dplyr::case_when(
+        site == "flow" ~ gage.string,
+        site == "recess_lag" ~ paste(gage.string, "recess_lag", sep = "_"),
+        TRUE ~ "ERROR"))
   
-  names(final.df)[names(final.df) %in% "recess_lag"] <- paste(gage.string, "recess_lag", sep = "_")
+  final.df <- dplyr::bind_rows(lag.df,
+                               dplyr::filter(flow.df, !site %in% gage.string))
   return(final.df)
 }
