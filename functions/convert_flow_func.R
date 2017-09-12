29aae245adb7f25c84e776806ce9ec5fb1e22d62
Zachary Smith <zsmith@ICPRB.INT>
2017-09-12 15:52:50 -0400

    Made flow.units an input value.

--- functions/convert_flow_func.R
+++ functions/convert_flow_func.R
@@ -1,16 +1,17 @@
 # Conversion factor for CFS to MGD.
 conversion.factor <- 1.5472286365101
 #------------------------------------------------------------------------------
-convert_flow <- function(data.df, org.units) {
+convert_flow <- function(data.df, org.units, flow.units) {
+  if (is.null(data.df)) return(NULL)
   if (org.units == "cfs") {
-    if (input$flow.units == "mgd") {
+    if (flow.units == "mgd") {
       final.df <- data.df %>% 
         mutate(flow = flow / conversion.factor)
     } else {
       final.df <- data.df
     }
   } else if (org.units == "mgd"){
-    if (input$flow.units == "cfs") {
+    if (flow.units == "cfs") {
       final.df <- data.df %>% 
         mutate(flow = flow * conversion.factor)
     } else {
