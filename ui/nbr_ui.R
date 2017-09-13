765b1634717333543442fcaf15bbb6a6fa55220d
Zachary Smith <zsmith@ICPRB.INT>
2017-09-13 09:16:43 -0400

    Attempted wellPanel for text output but decided against it. Left the script commented out incase we want to modify it and try again.

--- ui/nbr_ui.R
+++ ui/nbr_ui.R
@@ -1,10 +1,14 @@
 tabPanel("North Branch Release",
          fluidRow(
            align = "center",
-           plotOutput("nbr", height = plot.height, width = plot.width)
-         ), # fluidRow
-         fluidRow(
-           column(width = 12, offset = 1, align = "left", textOutput("nbr_notification_1")
-           ) # End column
+           plotOutput("nbr", height = plot.height, width = plot.width),
+           br(),
+           #wellPanel(
+           #  align = "middle",
+          #   style = "background-color: #ffffff;",
+             column(width = 12, offset = 0, align = "left",
+                    textOutput("nbr_notification_1")
+           )
+         #) # wellPanel
          ) # End fluidRow
 ) # End tabPanel
