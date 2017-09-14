83d19afa6c0710da2454dfa4880d938a4091e7cd
cheriels <cschultz@icprb.org>
2017-09-14 12:29:44 -0400

    updated current data thru 9/11

--- functions/constant_lagk_func.R
+++ functions/constant_lagk_func.R
@@ -4,8 +4,8 @@ constant_lagk <- function(flow.df, gage, todays.date, lag.days = 1) {
   todays.date <- lubridate::ymd(todays.date)
   gage <- rlang::enquo(gage)
   gage.string <- rlang::quo_name(gage)
-  if (gage.string == "por") gage.k <- 0.042
-  if (gage.string == "mon_jug") gage.k <- 0.050
+  if (gage.string == "por") gage.recessk <- 0.042
+  if (gage.string == "mon_jug") gage.recessk <- 0.050
   #----------------------------------------------------------------------------
   lag.df <- flow.df %>% 
     dplyr::select(date_time, site, flow) %>% 
@@ -16,7 +16,7 @@ constant_lagk <- function(flow.df, gage, todays.date, lag.days = 1) {
            recess_init = pmin(flow, flow1, flow2), # pmin gives minimums of each row
            recess_time = ifelse(date_time - todays.date < 0, 0, date_time - todays.date),
            recess = ifelse(date_time < todays.date, flow, 
-                           recess_init[date_time == todays.date] * exp(-gage.k * recess_time)),
+                           recess_init[date_time == todays.date] * exp(-gage.recessk * recess_time)),
            recess_lag = lag(recess, lag.days)) %>% 
     select(-flow1, -flow2, - recess_init, -recess_time, - recess, - site) %>% 
     tidyr::gather(site, flow, flow:recess_lag) %>% 
