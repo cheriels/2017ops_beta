6d98c07a51f7b29faa3b80ba9a0d05934f223cde
Zachary Smith <zsmith@ICPRB.INT>
2017-10-11 14:08:00 -0400

    Specified each of the variables to correct dts plot issues.

--- server/dts_server.R
+++ server/dts_server.R
@@ -71,10 +71,70 @@ output$dts <- renderPlot({
             min.flow = input$min.flow,
             max.flow = input$max.flow,
             gages.checked = input$gages.dts,
-            labels.vec = NULL,
-            linesize.vec = NULL,
-            linetype.vec = NULL,
-            color.vec = NULL,
+            labels.vec = c("WSSC Potomac River daily average withdrawals" = "WSSC Potomac River daily average withdrawals",
+                           "WSSC Patuxent Reservoirs daily average withdrawals" = "WSSC Patuxent Reservoirs daily average withdrawals",
+                           "WSSC Demand forecasted demand" = "WSSC Demand forecasted demand",
+                           
+                           "FW Potomac River daily average withdrawals" = "FW Potomac River daily average withdrawals",
+                           "FW Occoquan Reservoir daily average withdrawals" = "FW Occoquan Reservoir daily average withdrawals",
+                           "FW Demand forecasted demand" = "FW Demand forecasted demand",
+                           
+                           "WA Potomac River at Great Falls daily average withdrawals" = "WA Potomac River at Great Falls daily average withdrawals",
+                           "WA Potomac River at Little Falls daily average withdrawals" = "WA Potomac River at Little Falls daily average withdrawals",
+                           "WA Demand forecasted demand" = "WA Demand forecasted demand",
+                           
+                           "LW Broad Run daily discharge" = "LW Broad Run daily discharge",
+                           "LW Broad Run estimated average discharge" = "LW Broad Run estimated average discharge",
+                           
+                           "potomac_total" = "potomac_total"),
+            linesize.vec = c("WSSC Potomac River daily average withdrawals" = 2,
+                             "WSSC Patuxent Reservoirs daily average withdrawals" = 2,
+                             "WSSC Demand forecasted demand"  = 2,
+                             
+                             "FW Potomac River daily average withdrawals" = 	2,
+                             "FW Occoquan Reservoir daily average withdrawals" = 2,
+                             "FW Demand forecasted demand"  = 2,
+                             
+                             "WA Potomac River at Great Falls daily average withdrawals" = 2,
+                             "WA Potomac River at Little Falls daily average withdrawals" = 2,
+                             "WA Demand forecasted demand" = 	2,
+                             
+                             "LW Broad Run daily discharge" = 2,
+                             "LW Broad Run estimated average discharge" = 2,
+                             
+                             "potomac_total" = 	2),
+            linetype.vec = c("WSSC Potomac River daily average withdrawals" = "solid",
+                             "WSSC Patuxent Reservoirs daily average withdrawals" = "solid",
+                             "WSSC Demand forecasted demand"  = "solid",
+                             
+                             "FW Potomac River daily average withdrawals" = 	"solid",
+                             "FW Occoquan Reservoir daily average withdrawals" = "solid",
+                             "FW Demand forecasted demand"  = "solid",
+                             
+                             "WA Potomac River at Great Falls daily average withdrawals" = "solid",
+                             "WA Potomac River at Little Falls daily average withdrawals" = "solid",
+                             "WA Demand forecasted demand" = 	"solid",
+                             
+                             "LW Broad Run daily discharge" = "solid",
+                             "LW Broad Run estimated average discharge" = "solid",
+                             
+                             "potomac_total" = 	"solid"),
+            color.vec = c("WSSC Potomac River daily average withdrawals" = "#356478",
+                          "WSSC Patuxent Reservoirs daily average withdrawals" = "#4d8fac",
+                          "WSSC Demand forecasted demand"  = "#c9dde6",
+                          
+                          "FW Potomac River daily average withdrawals" = 	"#2e3967",
+                          "FW Occoquan Reservoir daily average withdrawals" = "#4d60ac",
+                          "FW Demand forecasted demand"  = "#b7bfdd",
+                          
+                          "WA Potomac River at Great Falls daily average withdrawals" = "#39672e",
+                          "WA Potomac River at Little Falls daily average withdrawals" = "#8fac4d",
+                          "WA Demand forecasted demand" = 	"#afd5a6",
+                          
+                          "LW Broad Run daily discharge" = "#783564",
+                          "LW Broad Run estimated average discharge" = "#ddb7d2",
+                          
+                          "potomac_total" = 	"#AC4D60"),
             x.class = "date",
             y.lab = y.units())
 }) # End output$dts
